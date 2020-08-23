d58acf658715d68dac8d1843fd7e754443f4ab97
Jayalekha Ramesh <jramesh@temenos.com>
2020-08-23 17:15:06 +1000

    feat(Setup): Initial react redux set up

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import {
+  createStore,
+  applyMiddleware,
+  compose
+} from "redux";
+import baseSaga from "../saga/rootSaga";
+import downloadSaga from "../saga/download";
+import userReducer from "../reducers/usersReducer";
+import createSagaMiddleware from "redux-saga";
+import dynamicSagaHelper from "../utils/dynamicSagaHelper";
 
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+
+const composeEnhancers =
+    typeof window === "object" &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        }) : compose;
+
+const rootSaga = dynamicSagaHelper(baseSaga, downloadSaga);
+
+const createStore = () => {
+
+    const sagaMiddleware = createSagaMiddleware();
+    const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
+    const store = createStore(userReducer, enhancer );
+    return { ...store,
+             runSaga:sagaMiddleware.run(rootSaga)
+    };
+};
+
+export default createStore;
